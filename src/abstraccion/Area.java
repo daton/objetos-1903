53d1600c44542de3f1e96bed4387bfa51051904d
campitos <rapidclimate@gmail.com>
2019-05-26 20:12:25 -0500

    aaaa

---
+++ src/abstraccion/Area.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstraccion;
+
+
+public class Area {
+    float resultado;
+    Figura figura;
+    
+    public void calcular(){
+      
+    }
+    
+}
