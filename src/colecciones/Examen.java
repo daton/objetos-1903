e4d2c75506930ad8c844b3f672a699a32eba2ea0
Juan Carlos Campos <rapidclimate@gmail.com>
2019-06-20 10:57:53 -0500

    asasasas

--- src/colecciones/Examen.java
+++ src/colecciones/Examen.java
@@ -37,11 +37,17 @@ public class Examen {
       Pregunta p1=new Pregunta();
       p1.setTitulo("Capital de Egipto");
       //ESto es lo interesante
-     
-      p1.setOpciones(Arrays.asList(op11,op21,op31,op41)); 
+      
+      List<Opcion> opcionesp1=new ArrayList<>();
+      opcionesp1.add(op11);
+      opcionesp1.add(op21);
+      opcionesp1.add(op31);
+      opcionesp1.add(op41);
+      p1.setOpciones(opcionesp1); 
+      //p1.setOpciones(Arrays.asList(op11,op21,op31,op41)); 
       //Para la pregunta 2
       Pregunta  p2=new Pregunta();
-      p2.setTitulo("Oceanos que rodean a México");
+      p2.setTitulo("Oceanos que rodean a Oaxaca");
       p2.setOpciones(Arrays.asList(op12, op22,op32,op42,op52));
            
       
@@ -56,6 +62,44 @@ public class Examen {
               System.out.println(o.getTitulo());
           }
       }
+     //Vamos a ver arreglos mutables de tipo List y Set
+     //Vamos a crear un arreglo de  tipo list
+        List<String>  lista=new ArrayList<>(); 
+       //Para llenarlo solo acepta enteros
+       lista.add("Juan");
+       lista.add("Ana");
+       lista.add("Pedro");
+       lista.add("Juan");
+       //Para ver el tamaño invocamos el método size()
+        System.out.println("Tamaño de la lista "+lista.size());
+       //Vamos a imprimirlo con un ciclo for mejorado
+        System.out.println("Impresion de la lista");
+        for(String i:lista){
+            System.out.println(i);
+        }
+        
+        //Ahora vamos a usar un set
+       Set<String> set=new HashSet<String>();
+       //Agregamos algunos elementos
+       set.add("Juan");
+       set.add("Ana");
+       set.add("Pedro");
+       set.add("Juan");
+       set.add("Irma");
+       //LO recorremos
+        System.out.println("Elementos del set");
+        for(String i:set){
+            System.out.println(i);
+        }
+        
+        Random r=new Random();
+     int x=   r.nextInt(10);
+        System.out.println("Numero aleatorio "+x);
+               
+       
+      
+      
+      
       
         
         
